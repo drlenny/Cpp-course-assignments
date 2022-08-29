56e270f128a0299e59e8535948512e5e016f534d
drlenny <66442932+drlenny@users.noreply.github.com>
2022-08-29 14:30:24 -0500

    reading from a text file part 2

---
+++ Section 19 IO and Streams/Read File/3/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+    ifstream in_file;
+
+    in_file.open("poem.txt");
+    if(!in_file){
+        cerr << "Problem opening file" << endl;
+        return 1;
+    }
+
+    string line;
+
+    while(getline(in_file, line)){
+        cout << line << endl;
+    }
+
+    in_file.close();
+    return 0;
+}
