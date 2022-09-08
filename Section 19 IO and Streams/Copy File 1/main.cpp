675e271df282c5ca7ea0223591272ce410f5168c
drlenny <66442932+drlenny@users.noreply.github.com>
2022-09-08 15:55:30 -0500

    writing text to a file

---
+++ Section 19 IO and Streams/Copy File 1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ifstream in_file{"poem.txt"};
+    ofstream out_file{"poem_out.txt"};
+    if (!in_file)
+    {
+        cerr << "Error opening input file" << endl;
+        return 1;
+    }
+    if (!out_file)
+    {
+        cerr << "Error opening output file" << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(in_file, line))
+        out_file << line << endl;
+
+    cout << "File copied" << endl;
+
+    in_file.close();
+    out_file.close();
+
+    return 0;
+}
---
+++ Section 19 IO and Streams/Write File/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    ofstream out_file{"output.txt", ios::app};
+    if(!out_file){
+        cerr << "Error creating file" << endl;
+        return 1;
+    }
+    string line;
+    cout << "Enter something to write to the file: ";
+    getline(cin, line);
+    out_file << line << endl;
+
+    out_file.close();
+
+    return 0;
+}
